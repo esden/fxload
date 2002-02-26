7052f387ca292d78495763c9d70e75c7837539c2
dbrownell <>
2002-02-26 19:55:05 +0000

    2nd stage loader support

--- ezusb.h
+++ ezusb.h
@@ -2,6 +2,7 @@
 #define __ezusb_H
 /*
  * Copyright (c) 2001 Stephen Williams (steve@icarus.com)
+ * Copyright (c) 2002 David Brownell (dbrownell@users.sourceforge.net)
  *
  *    This source code is free software; you can redistribute it
  *    and/or modify it in source code form under the terms of the GNU
@@ -20,17 +21,47 @@
  */
 #ident "$Id$"
 
+
 /*
- * This function loads into the opened EZUSB device the firmware in
- * the given file. The file is assumed to be in Intel HEX format, and
- * is loaded into the target memory literally.  If fx2 is set, uses
- * different reset commands.
+ * This function loads the firmware from the given file into RAM.
+ * The file is assumed to be in Intel HEX format.  If fx2 is set, uses
+ * appropriate reset commands.  Stage == 0 means this is a single stage
+ * load (or the first of two stages).  Otherwise it's the second of
+ * two stages; the caller preloaded the second stage loader.
  *
- * The target processor is reset as part of this load.
+ * The target processor is reset at the end of this download.
  */
-extern int ezusb_load_ihex(int dev, const char*path, int fx2);
+extern int ezusb_load_ram (int dev, const char *path, int fx2, int stage);
+
+
+/*
+ * This function stores the firmware from the given file into EEPROM.
+ * The file is assumed to be in Intel HEX format.  This uses the right
+ * CPUCS address to terminate the EEPROM load with a reset command,
+ * where FX parts behave differently than FX2 ones.  The configuration
+ * byte is as provided here, and the EEPROM type is set so that the
+ * microcontroller will boot from it.
+ * 
+ * The caller must have preloaded a second stage loader that knows
+ * how to respond to the EEPROM write request.
+ *
+ * NOTE:  this doesn't currently understand the original Anchorchips
+ * version of the EZ-USB, which uses a slightly different scheme for
+ * bootstrapping from EEPROMs.  Only the Cypress versions are supported.
+ */
+extern int ezusb_load_eeprom (int dev, const char *path, int fx2, int config);
+
+
+/* boolean flag, says whether to write extra messages to stderr */
+extern int verbose;
+
 
 /*
  * $Log$
+ * Revision 1.1  2001/06/12 00:00:50  stevewilliams
+ *  Added the fxload program.
+ *  Rework root makefile and hotplug.spec to install in prefix
+ *  location without need of spec file for install.
+ *
  */
 #endif
